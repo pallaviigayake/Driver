fd502c0ec3ebafbfd0495912f4f68b7285b541d5
Pallavi Gayake <maitri.pallavi32@gmail.com>
2020-08-08 21:19:43 +0530

    msg

--- app/src/androidTest/java/com/example/driver/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/driver/ExampleInstrumentedTest.java
@@ -2,13 +2,13 @@ package com.example.driver;
 
 import android.content.Context;
 
-import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -18,9 +18,10 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() {
+    public void useAppContext() throws Exception {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.driver", appContext.getPackageName());
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.lcn", appContext.getPackageName());
     }
 }
